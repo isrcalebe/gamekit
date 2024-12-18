a5068ac5aab69486740faceac4c8c314eaa46646
isrcalebe <work@isrcalebe.cc>
2024-12-18 11:38:41 -0300

    chore: remove samples

--- source/samples/shapes/source/resources/FKSampleAssemblyProvider.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-
-namespace FKSample.Game.Resources;
-
-public static class FKSampleResourceAssemblyProvider
-{
-    public static Assembly Assembly => typeof(FKSampleResourceAssemblyProvider).Assembly;
-}
